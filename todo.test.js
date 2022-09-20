ddb2dea4723dea908fa1ec651e4b348f28afc344
GeorginaNyamedi <ginamedi38@gmail.com>
2022-09-20 14:43:59 +0000

    first commit

---
+++ todo.test.js
@@ -0,0 +1,18 @@
+const { fetchData } = require("./fetchData");
+
+describe('Testing Post', () => {
+    it("should return 100 posts", async () => {
+        const posts = await fetchData("posts");
+        expect(posts.length).toBe(100);
+    });
+
+    it("should contain post with ID of 5", async () => {
+        const posts = await fetchData("posts")
+        expect(posts).toContainEqual({
+          userId: 1,
+          id: 1,
+          title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        });
+    });
+});
